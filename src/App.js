49739da13c6aa5f483227a01701147b1ae112885
ikazui <carmine.bodtlaender@gmail.com>
2022-12-01 22:33:38 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl font-bold underline">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+      sapiente repudiandae dolorem deserunt vitae fugit debitis tenetur, nobis a
+      molestiae odio minima atque, odit, perspiciatis totam delectus. Sit, ipsa
+      nostrum? Asperiores odio ab, quae assumenda iusto perspiciatis repudiandae
+      deleniti, ipsam omnis tempore adipisci ex totam, veritatis ipsa sit
+      obcaecati accusamus. Iste temporibus velit quos amet laudantium, magni
+      deleniti necessitatibus consectetur. Est iusto reiciendis, voluptate qui
+      praesentium, similique magni voluptates corrupti, exercitationem facere
+      id. Tenetur sint sequi similique soluta quos ipsum, quod minus ut nam
+      dolorum impedit architecto consequatur, pariatur libero! Labore,
+      dignissimos obcaecati minus exercitationem dicta, facere error quia libero
+      ex autem iste necessitatibus maxime dolore! Voluptatibus laudantium neque,
+      officiis enim alias quia nihil, provident minus autem quibusdam, nisi
+      consequuntur! Dignissimos neque cumque tenetur numquam ex reiciendis, ad
+      atque iure provident nobis amet fugiat? Quisquam error excepturi quasi
+      exercitationem alias nulla id laudantium doloremque, consequatur vel
+      sapiente cum harum at! Laboriosam harum quas consequatur, porro eveniet
+      quia eaque explicabo ipsa eligendi recusandae repudiandae quod nobis vitae
+      optio adipisci autem numquam maxime similique nisi aliquam labore dolore
+      vel. Ut, adipisci soluta. Incidunt expedita dolores soluta praesentium
+      enim consequuntur voluptatum, nemo sint animi architecto eos magni
+      sapiente vero, dicta nihil fugit doloremque deserunt quisquam tempora
+      laboriosam tempore culpa. Temporibus delectus nam soluta! Unde ex error
+      tempora mollitia, vero amet accusamus quia. Ratione culpa eius, in qui
+      delectus accusantium animi illo ut aut sit recusandae, expedita illum,
+      minima fugit odio sequi! Similique, ad? Eius veniam eum et consequuntur
+      iste? Quas harum aut explicabo veniam nobis, quod ea laborum vitae
+      corporis quidem numquam! Dolorum illo voluptatibus consectetur, quaerat
+      non repellendus inventore corporis rem in! Deleniti, aperiam pariatur.
+      Labore eaque, ut vel ea corporis assumenda aliquid repellat distinctio
+      praesentium veritatis et consequuntur sed cumque voluptas mollitia. Nisi
+      enim consectetur et commodi qui suscipit molestias accusamus.
     </div>
   );
 }
