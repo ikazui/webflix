fe0ef9560a5452dee10225cb8fb053d0e537734b
ikazui <carmine.bodtlaender@gmail.com>
2022-12-04 11:21:38 +0100

    dc

---
+++ src/API/TMDB.js
@@ -0,0 +1,67 @@
+const apiKey = "?api_key=490a9c8a065b325a7dd06b955be214d4";
+const apiBase = "https://api.themoviedb.org/3";
+export const imgBase = "https://image.tmdb.org/t/p/w500";
+
+const apiEndpointUpcomingMovies = "/movie/upcoming";
+
+const apiEndpointSearchTV = "/search/tv";
+
+/**
+ *
+ * @param "&language=en-US" language - Use translated data for fields that support it default en-US
+ * @param "&page=1" page - Which page to Query default 1
+ * @param "&region=DE" region - get Country specific Release Date
+ * @returns {   page: number,
+ *              results:[
+ *                          adult: boolean,
+ *                          backdrop_path: String imgPath,
+ *                          genre_ids: [numbers],
+ *                          id: int,
+ *                          original_language: "en",
+ *                          original_title: String,
+ *                          overview: String(description),
+ *                          popularity: number,
+ *                          poster_path: String,
+ *                          release_date: String;
+ *                          title: String,
+ *                          video: boolean,
+ *                          vote_average: number,
+ *                          vote_count: number],
+ *              total_pages: int,
+ *              total_results: int}
+ */
+export async function getUpcomingMovies(language, page, region) {
+  let url = [apiBase, apiEndpointUpcomingMovies, apiKey, language, page, region]
+    .filter((element) => typeof element === "string")
+    .join("");
+  const res = await fetch(url);
+  const data = await res.json("");
+  return data;
+}
+
+/**
+not done
+
+überleg ein object zu übergeben und das zu verwenden,
+ggf dynamisch genug, dass  ich eine funktion für alles verwenden kann#ifelse
+ */
+export async function getSearchMovies(searchOBj) {
+  const apiEndpointSearchMovie = "/search/movie";
+  const esc = encodeURIComponent;
+  const query = Object.keys(searchOBj)
+    .map((key) => esc(key) + "=" + esc(searchOBj[key]))
+    .join("&");
+  console.log(query);
+  const url = [apiBase, apiEndpointSearchMovie, apiKey, query].join("");
+  console.log(url);
+  const res = await fetch(url);
+  return await res.json("");
+}
+
+export async function searchFunction(type, searchObj) {
+  if (type === "movies") {
+    return await getSearchMovies(searchObj);
+  } else if (type === "upcomingMovies") {
+    return await getUpcomingMovies();
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,42 +1,93 @@
 import "./App.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { searchFunction } from "./API/TMDB";
 
 function App() {
+  const [navSearch, setNavSearch] = useState({ type: "movies", query: "" });
+
+  function handleChangeNavSearch(e) {
+    setNavSearch((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  }
+
+  function handleSearchSubmit(e) {
+    e.preventDefault();
+    console.log(e.target);
+    //searchFunction(e.target.name);
+  }
+
   return (
-    <div className="text-3xl font-bold underline">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-      sapiente repudiandae dolorem deserunt vitae fugit debitis tenetur, nobis a
-      molestiae odio minima atque, odit, perspiciatis totam delectus. Sit, ipsa
-      nostrum? Asperiores odio ab, quae assumenda iusto perspiciatis repudiandae
-      deleniti, ipsam omnis tempore adipisci ex totam, veritatis ipsa sit
-      obcaecati accusamus. Iste temporibus velit quos amet laudantium, magni
-      deleniti necessitatibus consectetur. Est iusto reiciendis, voluptate qui
-      praesentium, similique magni voluptates corrupti, exercitationem facere
-      id. Tenetur sint sequi similique soluta quos ipsum, quod minus ut nam
-      dolorum impedit architecto consequatur, pariatur libero! Labore,
-      dignissimos obcaecati minus exercitationem dicta, facere error quia libero
-      ex autem iste necessitatibus maxime dolore! Voluptatibus laudantium neque,
-      officiis enim alias quia nihil, provident minus autem quibusdam, nisi
-      consequuntur! Dignissimos neque cumque tenetur numquam ex reiciendis, ad
-      atque iure provident nobis amet fugiat? Quisquam error excepturi quasi
-      exercitationem alias nulla id laudantium doloremque, consequatur vel
-      sapiente cum harum at! Laboriosam harum quas consequatur, porro eveniet
-      quia eaque explicabo ipsa eligendi recusandae repudiandae quod nobis vitae
-      optio adipisci autem numquam maxime similique nisi aliquam labore dolore
-      vel. Ut, adipisci soluta. Incidunt expedita dolores soluta praesentium
-      enim consequuntur voluptatum, nemo sint animi architecto eos magni
-      sapiente vero, dicta nihil fugit doloremque deserunt quisquam tempora
-      laboriosam tempore culpa. Temporibus delectus nam soluta! Unde ex error
-      tempora mollitia, vero amet accusamus quia. Ratione culpa eius, in qui
-      delectus accusantium animi illo ut aut sit recusandae, expedita illum,
-      minima fugit odio sequi! Similique, ad? Eius veniam eum et consequuntur
-      iste? Quas harum aut explicabo veniam nobis, quod ea laborum vitae
-      corporis quidem numquam! Dolorum illo voluptatibus consectetur, quaerat
-      non repellendus inventore corporis rem in! Deleniti, aperiam pariatur.
-      Labore eaque, ut vel ea corporis assumenda aliquid repellat distinctio
-      praesentium veritatis et consequuntur sed cumque voluptas mollitia. Nisi
-      enim consectetur et commodi qui suscipit molestias accusamus.
+    <div className="bg-green-900">
+      <header className="flex border border-blue-100 fixed w-screen top-0 bg-green-500">
+        <div className="headerLogo">Test</div>
+        <nav className="flex gap-1">
+          <Link to={""}>Home</Link>
+          <Link to={""}>Filme</Link>
+          <Link to={""}>Serien</Link>
+          <Link to={""}>Random</Link>
+          <Link to={""}>Advanced Search</Link>
+        </nav>
+
+        <form action="" name="navSearch" onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            value={navSearch.query}
+            name="query"
+            onChange={handleChangeNavSearch}
+          />
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded">
+            bdfbdflbm
+          </button>
+        </form>
+      </header>
+
+      <main className="h-96">
+        {/** sprach selector mit map erstellen */}
+        <form
+          name=""
+          action=""
+          className="flex flex-col gap-2 mt-10"
+          onSubmit={handleSearchSubmit}
+        >
+          <label>
+            Search*:{" "}
+            <input type="text" name="querry" className="w-full" required />
+          </label>
+          <label>
+            Language:{" "}
+            <select name="language">
+              <option value="">fbfdbfdb</option>
+            </select>
+          </label>
+          <label>
+            Seite
+            <input type="number" name="page" />
+          </label>
+
+          <label className="hover:cursor-pointer">
+            Include Adult <input type="checkbox" name="include_adult" />
+          </label>
+          <label>
+            Region:{" "}
+            <select name="region">
+              <option value="">fbfdbfdb</option>
+            </select>
+          </label>
+          <button>bfdb</button>
+          {/*Select year by mapping over array to generate select options*/}
+        </form>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+/**
+
+          language, querry, page, include_adult, region, year,
+          primary_release_year
+
+*/
